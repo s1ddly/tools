4ec8331fba8e1d6909562084e3c08122011f406d
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2024-03-24 14:10:35 +1100

    got the bpm logic running

--- js/bpm.js
+++ js/bpm.js
@@ -1,4 +1,24 @@
 document.onkeypress = function (e) {
     e = e || window.event;
-    alert("key Pressed")
+    //alert("key Pressed")
+	render_bpm(calc_bpm());
 };
+
+var last_click = null;
+
+function calc_bpm(){
+	curdate = new Date()
+	if(last_click == null ){
+		last_click = curdate.getTime();
+		return 1;
+	} else {
+		curr_time = curdate.getTime() - last_click;
+		last_click = curdate.getTime();
+		return Math.round(60000/curr_time);
+	}
+}
+
+function render_bpm(num){
+	document.getElementById("bpm-here").innerText=num;
+	return null;
+}
