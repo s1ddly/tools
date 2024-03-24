810b3e194cf48d87769733ebdb8265e43a6a85e0
Sid Shardanand <sidthelizard@gmail.com>
2024-03-24 22:10:57 +1100

    added history functionality

--- assets/js/bpm.js
+++ assets/js/bpm.js
@@ -10,17 +10,18 @@ function calc_bpm(){
 	curdate = new Date()
 	if(last_click == null ){
 		last_click = curdate.getTime();
-		hist.push(last_click)
 		return "One more!";
 	} else {
 		curr_time = curdate.getTime() - last_click;
 		last_click = curdate.getTime();
-		hist.push(last_click)
-		return Math.round(60000/curr_time);
+		outval = Math.round(60000/curr_time);
+		hist.push(outval);
+		return outval;
 	}
 }
 
 function render_bpm(num){
 	document.getElementById("bpm-here").innerText=num;
+	document.getElementById("history-display").innerText=hist.join("\n");
 	return null;
 }
